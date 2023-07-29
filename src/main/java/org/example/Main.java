fe4d2bececfe993c5699c63fb1e4caefd384c99d
Semyon Kirekov <semyon@kirekov.com>
2023-07-29 17:59:15 +0300

    поправил Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,10 @@ package org.example;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
 import org.example.controller.BookController;
+import org.example.controller.BookFreemarkerController;
 import org.example.repository.InMemoryBookRepository;
 import org.example.service.BookService;
+import org.example.template.TemplateFactory;
 import spark.Service;
 
 public class Main {
@@ -12,14 +14,20 @@ public class Main {
   public static void main(String[] args) {
     Service service = Service.ignite();
     ObjectMapper objectMapper = new ObjectMapper();
+    final var bookService = new BookService(
+        new InMemoryBookRepository()
+    );
     Application application = new Application(
         List.of(
             new BookController(
                 service,
-                new BookService(
-                    new InMemoryBookRepository()
-                ),
+                bookService,
                 objectMapper
+            ),
+            new BookFreemarkerController(
+                service,
+                bookService,
+                TemplateFactory.freeMarkerEngine()
             )
         )
     );
